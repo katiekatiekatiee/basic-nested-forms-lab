5ae25798c3cc02cca0c0ddd104c62170fe88674f
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-07-16 17:28:56 -0400

    commit tree 1c39d1343788472dad66551484659d1880ee28fb

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,9 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+#   def ingredient_attributes=(ingredient)
+#     byebug
+#     self.ingredient = Ingredient.find_or_create_by(name: ingredient[:name])
+#   end
 end
